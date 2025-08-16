d87d990f035bea1f5d9b399a345f53fc93e226ab
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:26 +0530

    Initial commit: Portfolio-Website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Portfolio-Website"</h1>
+        <p>"Professional showcase"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
